31e73cc7b1e0629d88271a0c26b1ec5726950cb8
Mickke <mikaecat@gmail.com>
2017-05-14 18:24:45 +0700

    Add secondary screen

---
+++ Assets/Scripts/ActivateCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Display.displays[1].Activate();
+    }
+}
