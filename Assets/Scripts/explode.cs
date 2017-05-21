2954045082fd2bdd89705c63feefdc9f66777b14
korranat9 <kao9korrant@gmail.com>
2017-05-21 14:04:31 +0700

    add grenade effect2

--- Assets/Scripts/explode.cs
+++ Assets/Scripts/explode.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class explode : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +13,14 @@ public class explode : MonoBehaviour {
 	void Update () {
 		
 	}
+	void OnTriggerEnter2D(Collider2D collision){
+		Debug.Log ("OnTriggerEnter2D");
+		if (collision.gameObject.name == "enemy") {
+			Enemy e = collision.gameObject.GetComponent<Enemy>();
+			e.EnemyStun ();
+
+
+		}
+	}
+
 }
